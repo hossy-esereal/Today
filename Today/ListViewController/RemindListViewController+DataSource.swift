4dd52d6e6ffb02b401f33507c3bb883241989bc9
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-05-10 01:28:59 +0900

    DataSourceをViewControllerから分離

---
+++ Today/ListViewController/RemindListViewController+DataSource.swift
@@ -0,0 +1,24 @@
+//
+//  RemindListViewController+DataSource.swift
+//  Today
+//
+//  Created by hossy on 2022/05/10.
+//
+
+import UIKit
+
+@available(iOS 15.0, *)
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    @available(iOS 15.0, *)
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+    }
+}
