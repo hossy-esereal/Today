417f6620f8e8831afb0a141da116ddffe4fb9aab
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-05-11 00:09:24 +0900

    スナップショットを更新できるように

--- Today/ListViewController/RemindListViewController+DataSource.swift
+++ Today/ListViewController/RemindListViewController+DataSource.swift
@@ -13,6 +13,16 @@ extension ReminderListViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
     
+    func updateSnapshot(reloading ids: [Reminder.ID] = []) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(reminders.map { $0.id } )
+        if !ids.isEmpty {
+            snapshot.reloadItems(ids)
+        }
+        dataSource.apply(snapshot)
+    }
+    
     @available(iOS 15.0, *)
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
         let reminder = reminder(for: id)
@@ -35,6 +45,7 @@ extension ReminderListViewController {
         var reminder = reminder(for: id)
         reminder.isComplete.toggle()
         update(reminder, with: id)
+        updateSnapshot(reloading: [id])
     }
     
     private func doneButtonConfigutation(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
