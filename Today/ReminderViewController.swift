7e523bcfb861eb4a794302879a3ee0fe2b7e5291
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-05-11 12:26:21 +0900

    データソースの設定

--- Today/ReminderViewController.swift
+++ Today/ReminderViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
+@available(iOS 15.0, *)
 class ReminderViewController: UICollectionViewController {
+    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    
     var reminder: Reminder
+    private var dataSource: DataSource!
     
     init(reminder: Reminder) {
         self.reminder = reminder
@@ -20,5 +24,31 @@ class ReminderViewController: UICollectionViewController {
     
     required init?(coder: NSCoder) {
         fatalError("Always initialize ReminderViewController using init(reminde:)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Row) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+    }
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        cell.contentConfiguration = contentConfiguration
+        cell.tintColor = .todayPrimaryTint
+    }
+    
+    func text(for row: Row) -> String? {
+        switch row {
+        case .viewDate: return reminder.dueDate.dayText
+        case .viewNotes: return reminder.notes
+        case .viewTime: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .viewTitle: return reminder.title
+        }
     }
 }
