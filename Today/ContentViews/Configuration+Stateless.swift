ccbaa1896269eae199d0a3575d722ff5a70bc63d
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-05-12 20:06:40 +0900

    UIContentConfiguration追加

---
+++ Today/ContentViews/Configuration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration+Stateless.swift
+//  Today
+//
+//  Created by hossy on 2022/05/12.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
